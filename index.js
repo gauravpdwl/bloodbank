27f589f35c7ea5e69a423aea6a6fee4e62426a6e
Gaurav <gauravpadwal45@gmail.com>
2024-04-07 00:47:28 +0530

    created root route and welcome.ejs file

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const port=8000;
+const path=require('path');
+
+const app=express();
+
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    // const serverIp = req.socket.localAddress;
+    // console.log(serverIp);
+    res.render('welcome.ejs');
+});
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log("Error in Launching Application ", err);
+    }
+
+    console.log("Application is up and running on port ",port);
+});
